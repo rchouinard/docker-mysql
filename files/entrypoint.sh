bff85dfcf024e1b075faa76185387f9d28beb061
Ryan Chouinard <rchouinard@gmail.com>
2014-09-27 23:54:48 -0400

    Update entrypoint

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+#
+# Container entrypoint
+#
+# This file is executed when the container is run. It is responsible for
+# setting up the environment before the requested command is executed.
 
 MYSQL_LIB="/var/lib/mysql"
 MYSQL_RUN="/var/run/mysqld"
 
+# Create a directory owned by mysql
 createDir() {
 	DIR=$1
 	mkdir -p "$DIR"
@@ -10,44 +16,58 @@ createDir() {
 	chmod 0755 "$DIR"
 }
 
+# Execute a mysql query
+executeQuery() {
+	QUERY=$1
+	mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" --execute="$QUERY"
+	return $?
+}
+
+# Check for the mysql run directory
 if [ ! -d "$MYSQL_RUN" -a ! -L "$MYSQL_RUN" ];
 then
 	createDir "$MYSQL_RUN"
 fi
 
+# Check for the mysql data directory
 if [ ! -d "$MYSQL_LIB" -a ! -L "$MYSQL_LIB" ];
 then
 	createDir "$MYSQL_LIB"
 fi
 
+# If the data directory is empty and the command is mysqld or mysqld_safe...
 if [ -z "$(ls -A $MYSQL_LIB)" -a "${1%_safe}" = "mysqld" ];
 then
+
+	# Check if we have a password
 	if [ -z "$MYSQL_ROOT_PASSWORD" ];
 	then
 		echo >&2 "ERROR: Database is uninitialized and MYSQL_ROOT_PASSWORD not set"
 		exit 1
 	fi
 
-	mysql_install_db --user=mysql --datadir="$MYSQL_LIB" --skip-random-passwords
-	chown -R mysql:mysql "$MYSQL_LIB"
+	# Initialize the data directory and start the mysql daemon
+	mysql_install_db --user="mysql" --datadir="$MYSQL_LIB" --basedir="/usr"
+	mysqld --user="mysql" --disconnect_on_expired_password="OFF" > /dev/null &
 
-	mysqld --user=mysql > /dev/null &
-
+	# Wait for the mysql daemon to start up and change the root password
 	RET=1
 	while [[ $RET -ne 0 ]];
 	do
 		sleep 3
-		mysql --user=root --execute="STATUS" > /dev/null
+		mysql --user="root" --password="$(tail -n1 /root/.mysql_secret)" --execute="SET PASSWORD = PASSWORD('$MYSQL_ROOT_PASSWORD')" > /dev/null
 		RET=$?
 	done
 
-	mysql --user=root --execute="DELETE FROM mysql.user WHERE User='';"
-	mysql --user=root --execute="UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD'), password_expired='N' WHERE User='root';"
-	mysql --user=root --execute="CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-	mysql --user=root --execute="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
-	mysql --user=root --execute="DROP SCHEMA IF EXISTS \`test\`;"
+	# Remove anonymous user, create global root user, and drop test schema
+	executeQuery "DELETE FROM mysql.user WHERE User='';"
+	executeQuery "CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+	executeQuery "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+	executeQuery "DROP SCHEMA IF EXISTS \`test\`;"
 
-	mysqladmin --user=root shutdown
+	# Shutdown the mysql daemon
+	mysqladmin --user="root" --password="$MYSQL_ROOT_PASSWORD" shutdown
 fi
 
+# Execute the command
 exec "$@"
