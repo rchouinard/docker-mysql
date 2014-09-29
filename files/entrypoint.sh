c99a57dc77eda0ebb074fcfaa64198b5b9204e48
Ryan Chouinard <rchouinard@gmail.com>
2014-09-29 16:20:52 -0400

    Cleanup entrypoint script

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -7,32 +7,31 @@
 
 MYSQL_LIB="/var/lib/mysql"
 MYSQL_RUN="/var/run/mysqld"
+MYSQL_BASEDIR="/usr"
 
 # Create a directory owned by mysql
 createDir() {
-	DIR=$1
-	mkdir -p "$DIR"
-	chown mysql:mysql "$DIR"
-	chmod 0755 "$DIR"
+	mkdir -p "$1"
+	chown mysql:mysql "$1"
+	chmod $2 "$1"
 }
 
 # Execute a mysql query
 executeQuery() {
-	QUERY=$1
-	mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" --execute="$QUERY"
+	mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" --execute="$1" > /dev/null 2>&1
 	return $?
 }
 
 # Check for the mysql run directory
 if [ ! -d "$MYSQL_RUN" -a ! -L "$MYSQL_RUN" ];
 then
-	createDir "$MYSQL_RUN"
+	createDir "$MYSQL_RUN" 0755
 fi
 
 # Check for the mysql data directory
 if [ ! -d "$MYSQL_LIB" -a ! -L "$MYSQL_LIB" ];
 then
-	createDir "$MYSQL_LIB"
+	createDir "$MYSQL_LIB" 0700
 fi
 
 # If the data directory is empty and the command is mysqld or mysqld_safe...
@@ -47,26 +46,33 @@ then
 	fi
 
 	# Initialize the data directory and start the mysql daemon
-	mysql_install_db --user="mysql" --datadir="$MYSQL_LIB" --basedir="/usr"
+	mysql_install_db --user="mysql" --datadir="$MYSQL_LIB" --basedir="$MYSQL_BASEDIR"
 	mysqld --user="mysql" --disconnect_on_expired_password="OFF" > /dev/null &
 
 	# Wait for the mysql daemon to start up and change the root password
-	RET=1
-	while [[ $RET -ne 0 ]];
-	do
-		sleep 3
-		mysql --user="root" --password="$(tail -n1 /root/.mysql_secret)" --execute="SET PASSWORD = PASSWORD('$MYSQL_ROOT_PASSWORD')" > /dev/null
-		RET=$?
+	started=false
+	for i in {1..10}; do
+		mysqladmin ping > /dev/null 2>&1
+		if [[ $? -eq 0 ]]; then
+			started=true
+			break
+		fi
+		sleep 1
 	done
+	if [ "$started" != true ]; then
+		echo >&2 "ERROR: MySQL failed to start"
+		exit 1
+	fi
 
-	# Remove anonymous user, create global root user, and drop test schema
-	executeQuery "DELETE FROM mysql.user WHERE User='';"
+	# Reset root password
+	mysqladmin --user="root" --password="$(tail -n1 /root/.mysql_secret)" password "$MYSQL_ROOT_PASSWORD" > /dev/null 2>&1
+
+	# Create host-less root user
 	executeQuery "CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 	executeQuery "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
-	executeQuery "DROP SCHEMA IF EXISTS \`test\`;"
 
 	# Shutdown the mysql daemon
-	mysqladmin --user="root" --password="$MYSQL_ROOT_PASSWORD" shutdown
+	mysqladmin --user="root" --password="$MYSQL_ROOT_PASSWORD" shutdown > /dev/null 2>&1
 fi
 
 # Execute the command
