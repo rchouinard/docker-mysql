f16ec59ebbb66dac9cc93a67dbd27d52b925bcd6
Ryan Chouinard <rchouinard@gmail.com>
2014-09-29 18:21:49 -0400

    Cleanup entrypoint script

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -1,53 +1,79 @@
 #!/bin/bash
+#
+# Container entrypoint
+#
+# This file is executed when the container is run. It is responsible for
+# setting up the environment before the requested command is executed.
 
 MYSQL_LIB="/var/lib/mysql"
 MYSQL_RUN="/var/run/mysqld"
+MYSQL_BASEDIR="/usr"
 
+# Create a directory owned by mysql
 createDir() {
-	DIR=$1
-	mkdir -p "$DIR"
-	chown mysql:mysql "$DIR"
-	chmod 0755 "$DIR"
+	mkdir -p "$1"
+	chown mysql:mysql "$1"
+	chmod $2 "$1"
 }
 
+# Execute a mysql query
+executeQuery() {
+	mysql --user="root" --password="$MYSQL_ROOT_PASSWORD" --execute="$1" > /dev/null 2>&1
+	return $?
+}
+
+# Check for the mysql run directory
 if [ ! -d "$MYSQL_RUN" -a ! -L "$MYSQL_RUN" ];
 then
-	createDir "$MYSQL_RUN"
+	createDir "$MYSQL_RUN" 0755
 fi
 
+# Check for the mysql data directory
 if [ ! -d "$MYSQL_LIB" -a ! -L "$MYSQL_LIB" ];
 then
-	createDir "$MYSQL_LIB"
+	createDir "$MYSQL_LIB" 0700
 fi
 
+# If the data directory is empty and the command is mysqld or mysqld_safe...
 if [ -z "$(ls -A $MYSQL_LIB)" -a "${1%_safe}" = "mysqld" ];
 then
+
+	# Check if we have a password
 	if [ -z "$MYSQL_ROOT_PASSWORD" ];
 	then
 		echo >&2 "ERROR: Database is uninitialized and MYSQL_ROOT_PASSWORD not set"
 		exit 1
 	fi
 
-	mysql_install_db --user=mysql --datadir="$MYSQL_LIB"
-	chown -R mysql:mysql "$MYSQL_LIB"
+	# Initialize the data directory and start the mysql daemon
+	mysql_install_db --user="mysql" --datadir="$MYSQL_LIB" --basedir="$MYSQL_BASEDIR" --random-passwords
+	mysqld --user="mysql" --disconnect_on_expired_password="OFF" > /dev/null &
 
-	mysqld --user=mysql > /dev/null &
-
-	RET=1
-	while [[ $RET -ne 0 ]];
-	do
-		sleep 3
-		mysql --user=root --execute="STATUS" > /dev/null
-		RET=$?
+	# Wait for the mysql daemon to start up and change the root password
+	started=false
+	for i in {1..10}; do
+		mysqladmin ping > /dev/null 2>&1
+		if [[ $? -eq 0 ]]; then
+			started=true
+			break
+		fi
+		sleep 1
 	done
+	if [ "$started" != true ]; then
+		echo >&2 "ERROR: MySQL failed to start"
+		exit 1
+	fi
 
-	mysql --user=root --execute="DELETE FROM mysql.user WHERE User='';"
-	mysql --user=root --execute="UPDATE mysql.user SET Password=PASSWORD('$MYSQL_ROOT_PASSWORD'), password_expired='N' WHERE User='root';"
-	mysql --user=root --execute="CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-	mysql --user=root --execute="GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
-	mysql --user=root --execute="DROP SCHEMA IF EXISTS \`test\`;"
+	# Reset root password
+	mysqladmin --user="root" --password="$(cat /root/.mysql_secret | rev | cut -d' ' -f1 | rev)" password "$MYSQL_ROOT_PASSWORD" > /dev/null 2>&1
 
-	mysqladmin --user=root shutdown
+	# Create host-less root user
+	executeQuery "CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+	executeQuery "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+
+	# Shutdown the mysql daemon
+	mysqladmin --user="root" --password="$MYSQL_ROOT_PASSWORD" shutdown > /dev/null 2>&1
 fi
 
+# Execute the command
 exec "$@"
